173ccadb3ec646d7b299f60fff647b01a1d2d098
bhaskar253 <bhaskar.25392@gmail.com>
2018-05-25 21:23:23 +0530

    Add neMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from: 'robby@roayl.com',
+    text: 'Hello All',
+    createdAt: 214
+  });
+
+  socket.on('createMessage', function (message){
+    console.log('createMessage',message);
+  });
+
   socket.on('disconnect',()=>{
     console.log('Disconnected from client');
   });
