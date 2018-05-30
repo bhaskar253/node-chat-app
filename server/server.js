383bc4a19e23a45e1db64d9d246d1cf884c83ca4
bhaskar253 <bhaskar.25392@gmail.com>
2018-05-30 13:24:36 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection',(socket)=>{
   socket.on('createMessage', function (message,callback){
     console.log('createMessage',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
-    callback('This is');
+    callback();
     // socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
   });
 
